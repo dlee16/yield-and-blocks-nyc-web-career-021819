737f990cda7b0db44dc67edf62f340ee1fde25ba
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-05 14:24:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,13 @@
 def hello_t(array)
-i = 0 
-while i < array.length 
-yield(array[i])
-i +=1 
-end
-array
-end
+  if block_given?
+    i = 0 
+  
+    while i < array.length 
+      yield(array[i])
+      i +=1 
+    end
+    array
+  else
+    puts ""
 
 # call your method here!
